910c9f71e90958d2937ee2610e3badf31d1743f9
KarenJoos <karen.karebear@gmail.com>
2025-04-11 22:47:44 -0600

    order

---
+++ week04/OnlineOrdering/Order.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Reflection.Metadata.Ecma335;
+
+public class Order
+{
+    private List<Products> _products;
+    private Products _totalCost;
+    private Products _packingLabel;
+    private Customer _shippingLabel;
+
+    public Order
+    {
+        // List<Products> _products = new Products();
+
+        _totalCost = 
+
+
+    }
+}
